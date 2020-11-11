c75e035c509574b48df251d9e17aedbf956aad8a
Lola Monteil <lolamonteil@macbook-pro-de-lola.home>
2020-11-11 19:52:18 +0100

    refresh apres cors

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const { json } = require("express");
 const express = require("express");
+const cors = require("cors");
 const formidable = require("express-formidable");
 const mongoose = require("mongoose");
 const cloudinary = require("cloudinary").v2;
 
 const app = express();
 app.use(formidable());
+app.use(cors());
+
+require("dotenv").config();
+mongoose.connect(process.env.MONGODB_URI);
+app.listen(process.env.PORT, () => {
+  console.log("Server started");
+});
 
 mongoose.connect("mongodb://localhost/Vinted", {
   useNewUrlParser: true,
@@ -19,7 +27,6 @@ cloudinary.config({
   api_secret: "AEmEXr079dlpkYIHfOwfgDd4JNg",
 });
 
-// Import des routes
 const userRoutes = require("./routes/user");
 app.use(userRoutes);
 const offerRoutes = require("./routes/offer");
